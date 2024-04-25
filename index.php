6732905b734dfe8da150c92d17e97c6562fd8798
Alan <alan__rodrigues@hotmail.com.br>
2024-04-24 21:57:55 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,22 @@ require_once("config.php");
 
 // echo json_encode($usuarios);
 
-$root = new Usuario();
+//Carrega um usuário
+// $root = new Usuario();
+// $root->loadbyId(3);
+// echo $root;
 
-$root->loadbyId(3);
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+echo $usuario;
 
 ?>
